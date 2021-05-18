2e8bcb3dc3096b0b20e6a073445e40530b3c8127
Rodrigo Sousa <rodrigomras@gmail.com>
2021-05-18 21:00:53 +0100

    useful comment

--- predict_energy_generation/__init__.py
+++ predict_energy_generation/__init__.py
@@ -6,3 +6,5 @@ version_file = '{}/version.txt'.format(dirname(__file__))
 if isfile(version_file):
     with open(version_file) as version_file:
         __version__ = version_file.read().strip()
+
+# comment very useful
